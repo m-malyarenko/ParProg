ad8f62444abe111a0765fcf661407a0505629cd3
Mikhail Malyarenko <malyarenko.md@gmail.com>
2020-12-07 20:34:58 +0300

    lab_3 initial

---
+++ lab_3/src/main/java/ru/spbstu/telematics/malyarenko/lab_3/App.java
@@ -0,0 +1,13 @@
+package ru.spbstu.telematics.malyarenko.lab_3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
